ae313f780b947ec8745f1b790d333414ca01a487
xichuanwushao <avatarwx@163.com>
2023-12-08 17:56:53 +0800

    封装患者端登陆&注册服务层

---
+++ src/main/java/com/example/hospital/patient/wx/api/service/UserService.java
@@ -0,0 +1,12 @@
+package com.example.hospital.patient.wx.api.service;
+
+import java.util.HashMap;
+
+/**
+ * @author : wuxiao
+ * @date : 16:49 2023-12-08
+ */
+public interface UserService {
+    public HashMap loginOrRegister(String code, String nickname, String photo, String sex);
+
+}
